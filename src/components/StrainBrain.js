08feb967adeb5a6fa387012ec426987e38801fec
Travis Stevenson <thetechtravis@outlook.com>
2020-11-30 09:36:21 -0600

    Modified the catchphrase in StrainBrain.js

---
+++ src/components/StrainBrain.js
@@ -0,0 +1,14 @@
+/*
+Author: Travis Stevenson
+Purpose: This is the landing page that will be reached after user authentication.
+*/
+
+import React from "react"
+import "../components/StrainBrain.css"
+
+export const StrainBrain = () => (
+    <>
+        <h2>Strain Brain!</h2>
+        <small>Your personal guide to medical marijuana.</small>
+    </>
+)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { StrainBrain } from './components/StrainBrain';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StrainBrain />
   </React.StrictMode>,
   document.getElementById('root')
 );
