eb26ee921ba3455c7184ca5e25cccca5e3962f66
Travis Stevenson <thetechtravis@outlook.com>
2020-12-08 15:08:23 -0600

    Moved Navbar code into its own component

--- src/components/StrainBrain.js
+++ src/components/StrainBrain.js
@@ -8,34 +8,20 @@ import "../components/StrainBrain.css"
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
-import Navbar from "react-bootstrap/Navbar"
-import Nav from "react-bootstrap/Nav"
+// import Navbar from "react-bootstrap/Navbar"
+// import Nav from "react-bootstrap/Nav"
 import { StrainProvider } from "./strain/StrainProvider"
+import { StrainBrainNavbar } from "./navbar/StrainBrainNavbar"
 
 export const StrainBrain = () => (
     <>
-        <h2>Strain Brain!</h2>
-        <small>Your personal guide to medical marijuana.</small>
-
         <Route render={() => {
             // The user id is saved under the key app_user_id in local Storage. Change below if needed!
             if (localStorage.getItem("app_user_id")) {
                 return (
                     <>
                         {/* Components that are rendered when the user is authenticated go inside this React fragment */}
-
-                        {/* NAVBAR */}
-                        <Navbar bg="light" expand="lg">
-                            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                            <Nav className="mr-auto">
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#link">Conditions</Nav.Link>
-                                <Nav.Link href="#link">Puff</Nav.Link>
-                                <Nav.Link href="#link">Pass</Nav.Link>
-                            </Nav>
-                        </Navbar>
-
-                        <StrainProvider />
+                        <StrainBrainNavbar />
                     </>
                 )
             } else {
---
+++ src/components/navbar/StrainBrainNavbar.js
@@ -0,0 +1,20 @@
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+
+export const StrainBrainNavbar = (props) => {
+    return (
+        <>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">Strain Brain</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Conditions</Nav.Link>
+                    <Nav.Link href="#link">Puff</Nav.Link>
+                    <Nav.Link href="#link">Pass</Nav.Link>
+                </Nav>
+            </Navbar>
+            <h2>Strain Brain!</h2>
+            <small>Your personal guide to medical marijuana.</small>
+        </>
+    )
+}
