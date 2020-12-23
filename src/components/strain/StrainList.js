72e4c0dfd1563f1140b21a0b6a5f69f7e0d0ced5
Travis Stevenson <thetechtravis@outlook.com>
2020-12-23 17:43:35 -0600

    Added Loading message.

--- src/components/strain/StrainList.js
+++ src/components/strain/StrainList.js
@@ -28,39 +28,43 @@ export const StrainList = (props) => {
     const currentUser = localStorage.getItem("app_user_id")
     const activeUserConditions = userConditions.filter(uc => +currentUser === uc.userId)
     const activeUsersSetConditions = activeUserConditions.map(auc => auc.condition.name)
-
+    console.log(strains)
     return (
-        <>
-            <Accordion>
-                <Card>
-                    <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="0">Click for Help</Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="0">
-                        <Card.Body>Your strain recommendations are dependent on the medical conditons that you set in the "Conditions" tab.<br />Note: You will not see any strain recommendations until you set at least one medical condition.</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </Accordion>
-            <div className="strains" style={{ padding: "1rem" }}>
+        strains.length > 1900 ? (
+            <>
+                <Accordion>
+                    <Card>
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="link" eventKey="0">Click for Help</Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>Your strain recommendations are dependent on the medical conditons that you set in the "Conditions" tab.<br />Note: You will not see any strain recommendations until you set at least one medical condition.</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
+                <div className="strains" style={{ padding: "1rem" }}>
 
-                {
+                    {
 
-                    /* 
-                        Filter through array of every possible strain from API and only return
-                        strains which include at least one active user condition within medical
-                        properties of the current iterating strain. 
-    
-                        NOTE: In the future, lines 61 and 63 can be toggled with a button or
-                        some other affordance, so that the User can decide how their results
-                        are filtered.
-                    */
-                    // Line 61 -> renders strains that match ANY user conditions
-                    strains.filter(strain => strain.effects.medical.find(condition => activeUsersSetConditions.includes(condition)))
-                        // Line 63 -> renders strains that match ALL user conditions
-                        // strains.filter(strain => activeUsersSetConditions.every(condition => strain.effects.medical.includes(condition)))
-                        .map(strain => <Strain key={strain.id} strain={strain} />)
-                }
-            </div>
-        </>
+                        /* 
+                            Filter through array of every possible strain from API and only return
+                            strains which include at least one active user condition within medical
+                            properties of the current iterating strain. 
+        
+                            NOTE: In the future, lines 61 and 63 can be toggled with a button or
+                            some other affordance, so that the User can decide how their results
+                            are filtered.
+                        */
+                        // Line 61 -> renders strains that match ANY user conditions
+                        strains.filter(strain => strain.effects.medical.find(condition => activeUsersSetConditions.includes(condition)))
+                            // Line 63 -> renders strains that match ALL user conditions
+                            // strains.filter(strain => activeUsersSetConditions.every(condition => strain.effects.medical.includes(condition)))
+                            .map(strain => <Strain key={strain.id} strain={strain} />)
+                    }
+                </div>
+            </>
+        ) : (
+                <h1>Loading...</h1>
+            )
     )
 }
